84a86e2db9359e2fb7340c50068551199d4a62a6
khanhdoan-2809 <khanhdoan21.work@gmail.com>
2025-01-08 21:31:23 +0700

    add

--- ecommerce/ecommerce/src/main/java/com/cook/ecommerce/dao/IProductRepository.java
+++ ecommerce/ecommerce/src/main/java/com/cook/ecommerce/dao/IProductRepository.java
@@ -1,11 +1,16 @@
 package com.cook.ecommerce.dao;
 
 import com.cook.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "product", path="product")
 public interface IProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
