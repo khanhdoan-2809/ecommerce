e9053ccc78225469be57f9dfe241ca5631eaa5d7
khanhdoan-2809 <khanhdoan21.work@gmail.com>
2025-01-08 20:52:58 +0700

    update

--- ecommerce/ecommerce/src/main/java/com/cook/ecommerce/dao/IProductCategoryRepository.java
+++ ecommerce/ecommerce/src/main/java/com/cook/ecommerce/dao/IProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.cook.ecommerce.dao;
 import com.cook.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 // collectionResourceUrl: name of JSON entry
 @RepositoryRestResource(collectionResourceRel = "productCategory", path="product-category")
 public interface IProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
